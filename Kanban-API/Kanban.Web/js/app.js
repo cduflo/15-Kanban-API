b47122cb4d3aa0119d631b0cde53c93d5b527a7c
Chris Duflo <chris.duflo@gmail.com>
2016-02-04 12:26:01 -0800

    Initial Commit

---
+++ Kanban-API/Kanban.Web/js/app.js
@@ -0,0 +1,23 @@
+﻿angular.module('kanban', ['ngResource']);
+angular.module('kanban').value('apiUrl', 'http://localhost:52972/api');
+angular.module('kanban').controller('IndexController', function ($scope, $resource ,apiUrl) {
+    var Listresource = $resource(apiUrl + '/lists/:listID', { listID: '@id' });
+    var lists = Listresource.query();
+    $scope.lists = lists;
+
+    function activate() {
+        var lists = Listresource.query();
+        $scope.lists = lists;
+    };
+ 
+    $scope.newList = {};
+
+    $scope.addList = function () {
+        Listresource.save($scope.newList, function () {
+            alert('save successful');
+            activate();
+        });
+    };
+
+    activate();
+});
