41b10c08010e8ce569970423baf6d955f8e6919a
Chris Duflo <chris.duflo@gmail.com>
2016-02-04 16:56:31 -0800

    lists fully populated. troubleshooting deletions

--- Kanban-API/Kanban.Web/js/app.js
+++ Kanban-API/Kanban.Web/js/app.js
@@ -1,22 +1,53 @@
 ﻿angular.module('kanban', ['ngResource']);
 angular.module('kanban').value('apiUrl', 'http://localhost:52972/api');
 angular.module('kanban').controller('IndexController', function ($scope, $resource ,apiUrl) {
-    var Listresource = $resource(apiUrl + '/lists/:listID', { listID: '@id' });
-    var lists = Listresource.query();
-    $scope.lists = lists;
+    var ListResource = $resource(apiUrl + '/lists/:listID', { listID: '@listID' }, {
+        'cards': {
+            url: apiUrl + '/lists/:listID/cards',
+            method: 'GET',
+            isArray: true,
+        }
+    });
 
     function activate() {
-        var lists = Listresource.query();
-        $scope.lists = lists;
-    };
+        ListResource.query(function (data) {
+            $scope.lists = data;
+
+            $scope.lists.forEach(function (list) {
+                list.cards = ListResource.cards({ listID: list.ListID });
+            });
+        });
+    }
  
     $scope.newList = {};
+    $scope.newCard = {};
+    //$scope.dCard = {};
 
     $scope.addList = function () {
-        Listresource.save($scope.newList, function () {
+        ListResource.save($scope.newList, function () {
             alert('save successful');
             activate();
         });
+    };
+
+    $scope.addCard = function () {
+        ListResource.save($scope.newList, function () {
+            alert('save successful');
+            activate();
+        });
+    };
+
+    $scope.delList = function (list) {
+        list.$delete(function(){
+            activate();
+        })
+    };
+
+    $scope.delCard = function (card) {
+        ListResource.delete(card.cardID, function () {
+            alert('delete successful');
+            activate();
+        })
     };
 
     activate();
