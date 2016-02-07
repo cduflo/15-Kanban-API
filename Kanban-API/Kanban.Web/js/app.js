fadc3b5ad9e13948f5f47db45526cd6eadf0912b
Chris Duflo <chris.duflo@gmail.com>
2016-02-06 21:33:31 -0800

    cleanup

--- Kanban-API/Kanban.Web/js/app.js
+++ Kanban-API/Kanban.Web/js/app.js
@@ -74,16 +74,16 @@ angular.module('kanban').controller('IndexController', function ($scope, $resour
 
     activate();
 
-    var curYPos = 0,
-    curXPos = 0,
-    curDown = false;
+    //var curYPos = 0,
+    //curXPos = 0,
+    //curDown = false;
 
-    window.addEventListener('mousemove', function (e) {
-        if (curDown === true) {
-            window.scrollTo(document.body.scrollLeft + (curXPos - e.pageX), document.body.scrollTop + (curYPos - e.pageY));
-        }
-    });
+    //window.addEventListener('mousemove', function (e) {
+    //    if (curDown === true) {
+    //        window.scrollTo(document.body.scrollLeft + (curXPos - e.pageX), document.body.scrollTop + (curYPos - e.pageY));
+    //    }
+    //});
 
-    window.addEventListener('mousedown', function (e) { curDown = true; curYPos = e.pageY; curXPos = e.pageX; });
-    window.addEventListener('mouseup', function (e) { curDown = false; });
+    //window.addEventListener('mousedown', function (e) { curDown = true; curYPos = e.pageY; curXPos = e.pageX; });
+    //window.addEventListener('mouseup', function (e) { curDown = false; });
 });
